7b681178b994051c4864b8eab5dbfc49821b8465
norbikam <norbikamnorbert10@gmail.com>
2025-03-17 21:05:11 +0100

    Mobile devices fix
    Removed fixed background for mobile devices

--- app/components/MembercardInfo.tsx
+++ app/components/MembercardInfo.tsx
@@ -5,7 +5,7 @@ export default function MembercardInfo() {
     <section className="relative min-h-auto flex items-center justify-center px-4 py-12">
       {/* Background layer with fixed attachment for parallax effect */}
       <div
-        className="absolute inset-0 z-[-1] bg-cover bg-center bg-fixed"
+        className="absolute inset-0 z-[-1] bg-cover bg-center bg-fixed bg-no-repeat md:bg-scroll"
         style={{
           backgroundImage:
             "url(https://img.freepik.com/darmowe-zdjecie/rozmazana-buzka-kobieta-trzyma-wizytowke_23-2149343413.jpg?t=st=1742240040~exp=1742243640~hmac=a17e3cfebc8740be914aebc395871b36d583ef845c0a2623613545e2fd07b104&w=2000)",
