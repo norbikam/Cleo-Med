46d16febe6956d6f1ff43c0056ed3e2784fc2f54
norbikam <norbikamnorbert10@gmail.com>
2025-03-22 22:28:14 +0100

    Updated kontakt page

--- app/kontakt/page.tsx
+++ app/kontakt/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <header>
         <Navigation />
       </header>
-      <main style={{ maxWidth: "98vw" }} className="overflow-hidden pt-20 text-center">
+      <main style={{ width: "98vw" }} className="overflow-hidden pt-20 text-center">
         <h2 className="text-black text-2xl pt-6" style={{fontSize:"5rem"}}>Kontakt</h2>
         <h3 className="text-gray-500 pb-10 py-4 text-2xl">Umów się już dziś</h3>
         <div className="w-[98vw] flex flex-col md:grid md:grid-cols-5 md:gap-8 items-center text-black w-full bg-white">
@@ -18,13 +18,13 @@ export default function Home() {
             <h2 style={{fontSize:"4rem"}}>CleoMed</h2>
             <p style={{marginTop:"-1.5rem"}}>Gabinet kosmetyczny</p><br />
             <div className="flex flex-col justify-center text-xl w-full md:grid md:grid-cols-2 md:text-center">
-              <div className="flex flex-col sm:w-full"><p>ul. Wileńska 39 box 113</p><p>76-200 Słupsk</p></div>
+              <div className="flex flex-col sm:w-full"><p>ul. Wileńska 39, Manhattan, box 113</p><p>76-200 Słupsk</p></div>
               <div className="flex flex-col sm:w-full md:py-0 sm:py-10"><a href="mailto:edytacleo@gmail.com"><p>edytacleo@gmail.com</p></a><a href="tel:+48696273477"><p>+48 696 273 477</p></a></div>
               <br />
-              <div className="flex place-content-around col-span-2"><Facebook/><Instagram/><WhatsApp/></div>
+              <div className="flex place-content-around col-span-2 w-full"><Facebook/><Instagram/><WhatsApp/></div>
             </div>
             <br />
-            <iframe className="justify-self-center" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2318.3073038303396!2d17.0172382770225!3d54.46498239043967!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46fe10a824f679b9%3A0xef089efff4316619!2sC.H.%20Manhatan!5e0!3m2!1spl!2spl!4v1742386425730!5m2!1spl!2spl" width="800" height="450" loading="lazy"></iframe>
+            <iframe className="justify-self-center w-full ml-10" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2318.3073038303396!2d17.0172382770225!3d54.46498239043967!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46fe10a824f679b9%3A0xef089efff4316619!2sC.H.%20Manhatan!5e0!3m2!1spl!2spl!4v1742386425730!5m2!1spl!2spl" height="450" loading="lazy"></iframe>
             </div>
           <div className="splitimagetop w-full h-64 bg-gray-300 md:h-[85vh] col-span-2"></div>
         </div>
