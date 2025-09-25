6c6482570a860db34b5929e16b2601c506ac1a43
norbikam <norbikamnorbert10@gmail.com>
2025-09-25 11:41:41 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { useAuth } from './hooks/useAuth'; // Dostosuj ścieżkę jeśli trzeba
-import Image from 'next/image';
 
 interface Product {
   id: string;
